d65c5e73cb7c1df05165d533787a5bb08fa9899f
radusporniac <sporniacr@gmail.com>
2022-07-18 17:22:06 +0300

    Add project files.

---
+++ jwtWebApi/User.cs
@@ -0,0 +1,11 @@
+﻿namespace jwtWebApi
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;    
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+    }
+}
